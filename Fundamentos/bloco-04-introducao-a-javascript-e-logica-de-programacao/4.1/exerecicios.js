2c85fced78a5a1b38803c98bc477d4549e39e511
Fernando Sena <fernandosenacruz@gmail.com>
2021-07-28 16:23:35 -0300

    inicio do exercicio 4.1

---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/4.1/exerecicios.js
@@ -0,0 +1,29 @@
+/* Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b)*/
+var a = 5, b = 8;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log();
+
+/*Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.*/
+a= 10 , b = 20;
+
+if(a>b){
+    console.log(a);
+}else if(a<b) {
+    console.log(b)
+}else{
+    console.log(a + " é igual a " + b)
+}
+console.log();
+
+/*Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.*/
+
