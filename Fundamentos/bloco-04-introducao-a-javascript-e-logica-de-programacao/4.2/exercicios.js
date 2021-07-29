1e890fbe583888377dc99e75cfefc578afe37abf
Fernando Sena <fernandosenacruz@gmail.com>
2021-07-29 19:25:02 -0300

    questoes 4, 5 e 6 ok

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/4.2/exercicios.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/4.2/exercicios.js
@@ -2,17 +2,54 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // for (let index = 0; index <= numbers.length-1; index +=1){
 //     console.log(numbers[index]);
-// }  ok
+// }  1 ok
 
-let sum = 0;
+// let sum = 0;
 
-for (let index = 0; index <numbers.length; index +=1){
-    sum += numbers[index];
-}
-console.log(sum);
+// for (let index = 0; index <numbers.length; index +=1){
+//     sum += numbers[index];
+// }
+// console.log(sum); 2 ok
 
-// let sum = numbers.reduce(function(sum, numbers){
-//     return sum + numbers;
-// }, 0);
-// console.log(sum); outra forma de resolver a questão 2
+// // let sum = numbers.reduce(function(sum, numbers){
+// //     return sum + numbers;
+// // }, 0);
+// // console.log(sum); outra forma de resolver a questão 2
+
+// let sum = 0;
+
+// for (let index = 0; index <numbers.length; index +=1){
+//     sum += numbers[index];
+// }
+// let media = sum / numbers.length;
+// console.log(media); 3 ok
+
+// if (media >= 20){
+//     console.log("Valor maior que 20");
+// }else{
+//     console.log("Valor nemor que 20");
+// } 4 ok
+
+// let maxNumber = numbers[0];
+
+// for (let index = 0; index <= numbers.length-1; index +=1){
+//     if (numbers[index] > maxNumber){
+//         maxNumber = numbers[index];
+//     }
+// } 
+
+// console.log(maxNumber); 5 ok
+
+// let odd = 0;
+
+//  for (let index = 0; index <= numbers.length-1; index +=1){
+//      if (numbers[index] % 2 != 0){
+//          odd += 1;
+//      }
+// } 
+//     if (odd != 0){
+//         console.log(odd);
+//     }else{
+//         console.log("nenhum valor ímpar encontrado")
+//     } 6 ok
 
