33a1876fd9a7bb2de3d8360b31e42fc89fd8355d
Fernando Sena <fernandosenacruz@gmail.com>
2021-08-02 16:23:57 -0300

    exercise objetos e for/in.

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/4.4/exercise.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/4.4/exercise.js
@@ -3,3 +3,33 @@ let info = {
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
+
+console.log("Bem-vinda, " + info.personagem);
+
+info["recorrente"] = "sim";
+
+console.log(info);
+
+for (let key in info){
+    console.log(key);
+}
+console.log();
+
+for (let key in info){
+    console.log(info[key]);
+}
+console.log();
+
+for (let key in info){
+    console.log(key);
+}
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+};
+console.log();
+
+for (let key in info, info2){
+    console.log(info[key] + " e " + info2[key]);
+}
