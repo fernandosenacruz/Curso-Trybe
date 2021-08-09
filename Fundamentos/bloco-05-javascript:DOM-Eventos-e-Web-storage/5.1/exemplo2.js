f15f1f6161107e552e67df451d719128ba48c8b5
Fernando Sena <fernandosenacruz@gmail.com>
2021-08-09 16:39:45 -0300

    add exemplo2.html, exemplo2.css e exemplo2.js

---
+++ Fundamentos/bloco-05-javascript:DOM-Eventos-e-Web-storage/5.1/exemplo2.js
@@ -0,0 +1,7 @@
+document.querySelector("h1").style.color = "white";
+document.querySelector("h1").style.background = "green";
+document.querySelector(".emergency-tasks h3").style.background = "purple";
+document.querySelector(".emergency-tasks").style.background = "pink";
+document.querySelectorAll(".no-emergency-tasks h3").style.background = "yellow";
+document.querySelector(".no-emergency-tasks h3").style.background = "purple";
+document.querySelector(".no-emergency-tasks").style.background = "pink";
