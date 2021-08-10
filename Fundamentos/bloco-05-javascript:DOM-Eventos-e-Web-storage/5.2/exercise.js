1842010a143a7897d435bb8bb50a56808018562a
Fernando Sena <fernandosenacruz@gmail.com>
2021-08-10 15:45:26 -0300

    requisitos 1 e 2 ok

---
+++ Fundamentos/bloco-05-javascript:DOM-Eventos-e-Web-storage/5.2/exercise.js
@@ -0,0 +1,10 @@
+// requisito 1
+let body = document.getElementsByTagName("body")[0];
+body.appendChild(document.createElement("h1"));
+
+// requisito 2
+body.appendChild(document.createElement('main'));
+let main = document.getElementsByTagName('main')[0];
+main.className = "main-content";
+
+// requisito 3
