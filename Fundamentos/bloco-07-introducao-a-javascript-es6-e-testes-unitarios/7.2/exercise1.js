32816ad4c882b52de82d12e898777b8149efc54a
Fernando Sena <fernandosenacruz@gmail.com>
2021-08-30 18:03:51 -0300

    part I ok

--- Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/7.2/exercise1.js
+++ Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/7.2/exercise1.js
@@ -35,15 +35,15 @@ const order = {
 };
 
 const customerInfo = (order) => {
-  // Adicione abaixo as informações necessárias.
-
+  console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}.`)
 }
 
 customerInfo(order);
 
 const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
-
+    order.name = 'Luiz Silva';
+    order.payment.total = 50;
+    console.log(`Olá ${order.name}, o total do seu pedido de ${Object.keys(order.order.pizza).join(', ')} e ${order.order.drinks.coke.type}, é de R$ ${order.payment.total}.`)
 }
 
 orderModifier(order);
