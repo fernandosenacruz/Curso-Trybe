b4bcae8e4b7fa9dd63e4c7aa969778eac674c0e0
Fernando Sena <fernandosenacruz@gmail.com>
2021-09-06 15:01:01 -0300

    exercise 4 ok

---
+++ Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/8.3/exercise4.js
@@ -0,0 +1,35 @@
+const books = require("../8.2/arrayBase");
+const assert = require('assert');
+
+const expectedResult = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+];
+
+let date = new Date();
+
+function oldBooksOrdered() {
+  let releaseYearInfo = books.filter((book) => (date.getFullYear() - book.releaseYear) > 60);
+  return releaseYearInfo.sort((a, b) => a.releaseYear - b.releaseYear);
+}
+
+assert.deepStrictEqual(oldBooksOrdered(), expectedResult);
