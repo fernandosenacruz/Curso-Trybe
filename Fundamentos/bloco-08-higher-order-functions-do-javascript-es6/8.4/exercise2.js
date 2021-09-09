bdcfc6663818abe3e06c1ef14266dbeeccb53240
Fernando Sena <fernandosenacruz@gmail.com>
2021-09-09 18:33:51 -0300

    exercise2 ok

--- Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/8.4/exercise2.js
+++ Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/8.4/exercise2.js
@@ -1,13 +1,20 @@
 const assert = require("assert");
 
-const books = require('../8.2/arrayBase')
+const books = require("../8.2/arrayBase");
 
 //Crie uma string com os nomes de todos os autores.
 
-const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+const expectedResult =
+  "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
 
 function reduceNames() {
-  
+  return books.reduce((acc, cur, index) => {
+    if (index == books.length - 1) {
+      return (acc += `${cur.author.name}.`);
+    } else {
+      return (acc += `${cur.author.name}, `);
+    }
+  }, '');
 }
 
 assert.strictEqual(reduceNames(), expectedResult);
