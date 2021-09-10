044d55aa9ff3edef9763848f8050ad6bec9d8726
Fernando Sena <fernandosenacruz@gmail.com>
2021-09-10 18:44:59 -0300

    add exercises. exercise1 ok

---
+++ Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/8.5/exercise6.js
@@ -0,0 +1,4 @@
+/*6 - Suponha que você esteja lidando com carros e, da forma como o problema lhe foi entregue, 
+cada carro é modelado como um array. Porém, essa modelagem está baixo nível. 
+Cria uma função toObject que, dada uma lista, retorna um objeto representando o carro:
+Dica: use array destructuring e abbreviation object literal .*/
