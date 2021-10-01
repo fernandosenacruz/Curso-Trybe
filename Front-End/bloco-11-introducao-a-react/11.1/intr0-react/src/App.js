2bc18aa2656025453a93dd842967540939e19176
Fernando Sena <fernandosenacruz@gmail.com>
2021-10-01 14:47:50 -0300

    refatorando code

--- Front-End/bloco-11-introducao-a-react/11.1/intr0-react/src/App.js
+++ Front-End/bloco-11-introducao-a-react/11.1/intr0-react/src/App.js
@@ -1,20 +1,25 @@
-// import React, { Component } from "react";
-// import "./App.css";
-// import ReactClass from "./Component";
-// import Header from "./Header";
-// import Content from "./Content";
+import "./App.css";
+import ReactClass from "./Component";
+import Header from "./Header";
+import Content from "./Content";
 import Footer from "./Footer";
 
-// const Task = (value) => {
-//   return (
-//     <li>{value}</li>
-//   );
-// }
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
 
 function App() {
-  // const arr = ['estudar react', 'fazer os exercícios desta mera', 'chorar no banho'];
-  // return (Task(arr.map((e) => `${e}\n`)));
-  return(<Footer />);
+  const arr = ['estudar react', 'fazer os exercícios desta mera', 'chorar no banho'];
+  return(
+  <section>
+    <ReactClass/>
+    <Header />
+    {arr.map((e) => Task(`${e}`))}
+    <Content />
+    <Footer />
+  </section>)
 }
 
 export default App;
--- Front-End/bloco-11-introducao-a-react/11.1/intr0-react/src/Content.jsx
+++ Front-End/bloco-11-introducao-a-react/11.1/intr0-react/src/Content.jsx
@@ -1,36 +1,39 @@
-import React from "react";
+import React from 'react';
 
-class Content extends React.Component{
-    render(){
-        const conteudos = [
-  {
-    conteudo: 'High Order Functions',
-    bloco: 8,
-    status: 'Aprendido'
-  },
-  {
-    conteudo: 'Composicao de Componentes',
-    bloco: 11,
-    status: 'Aprendendo',
-  },
-  {
-    conteudo: 'Composicao de Estados',
-    bloco: 12,
-    status: 'Aprenderei'
-  },
-  {
-    conteudo: 'Redux',
-    bloco: 16,
-    status: 'Aprenderei'
-  },
-];
-        return (
-            conteudos.map((e) => 
-            `O conteúdo é: ${e.conteudo}\n 
-            Status: ${e.status}\n
-            Bloco: ${e.bloco}\n`)
-        )
-    }
+class Content extends React.Component {
+  render() {
+    const conteudos = [
+      {
+        conteudo: 'High Order Functions',
+        bloco: 8,
+        status: 'Aprendido',
+      },
+      {
+        conteudo: 'Composicao de Componentes',
+        bloco: 11,
+        status: 'Aprendendo',
+      },
+      {
+        conteudo: 'Composicao de Estados',
+        bloco: 12,
+        status: 'Aprenderei',
+      },
+      {
+        conteudo: 'Redux',
+        bloco: 16,
+        status: 'Aprenderei',
+      },
+    ];
+    return conteudos.map((e) => (
+      <p>
+        {`O conteúdo é: ${e.conteudo}`}
+        <br />
+        {`Status: ${e.status}`}
+        <br />
+        {`Bloco: ${e.bloco}`}
+      </p>
+    ));
+  }
 }
 
 export default Content;
