8f1f5d1f4295d666e6d291a3c100028f0fac4b65
Fernando Sena <fernandosenacruz@gmail.com>
2021-10-06 11:24:36 -0300

    fixaçao finalizado

--- Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/11.1/fixacao/src/App.js
+++ Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/11.1/fixacao/src/App.js
@@ -6,21 +6,27 @@ export default class App extends React.Component {
     super() 
     this.clickNoBotão = this.clickNoBotão.bind(this)
       this.state = {
-        clicks: 0
+        clicks: 0,
+        style: {color: "green"}
     }
   }
 
   clickNoBotão() {
-    this.setState((previousState, _props) => ({
-      clicks: previousState.clicks + 1
-    }))
+    this.setState((previousState, _props) => {
+      console.log(previousState.style.color)
+      const ximira = previousState.style.color === "green" ? "black" : "green"
+      return{
+        clicks: previousState.clicks + 1,
+        style: {color: ximira}
+      } 
+    })
   }
 
   render() {
     return (
       <div>
         <h1>Evento e Estado</h1>
-        <button onClick={ this.clickNoBotão}>Benjamim Button: { this.state.clicks }</button>
+        <button onClick={ this.clickNoBotão} style={{color: this.state.style.color}}>Benjamim Button: { this.state.clicks }</button>
       </div>
     )  
   }
