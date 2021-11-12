87052d04edff69acb9f9d16c4300d7f5164fe6ea
Fernando Sena <fernandosenacruz@gmail.com>
2021-11-11 23:21:21 -0300

    exercicio 1 ok

--- Front-End/bloco-15--gerenciamento-de-estado-com-redux/15.1/exercicio.js
+++ Front-End/bloco-15--gerenciamento-de-estado-com-redux/15.1/exercicio.js
@@ -2,6 +2,10 @@ import { createStore } from 'redux';
 
 const PREVIOUS_COLOR = 'PREVIOUS_COLOR';
 const NEXT_COLOR = 'NEXT_COLOR';
+const BTN_PREVIOUS_COLOR = document.getElementById('previous');
+const BTN_NEXT_COLOR = document.getElementById('next');
+const SPAN_COLOR = document.getElementById('value');
+
 
  const ESTADO_INICIAL = {
         colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
@@ -9,20 +13,32 @@ const NEXT_COLOR = 'NEXT_COLOR';
       };
 
 const reducer = (state = ESTADO_INICIAL, action) => {
+  const { index, colors } = ESTADO_INICIAL;
   switch (action.type) {
     case PREVIOUS_COLOR:
       return {
-        
+        ...state,
+        index: index === colors.length -1 ? index : index--,
       }
 
       case NEXT_COLOR:
       return {
-        
+        ...state,
+        index: index++ === colors.length ? index : index++,
       }
 
     default:
       return state;
   }
-}
+};
+
+console.log(BTN_PREVIOUS_COLOR)
+BTN_PREVIOUS_COLOR.addEventListener('click', () => { console.log('qualqur coisa')});
+BTN_NEXT_COLOR.addEventListener('click', () => { store.dispatch({ type: NEXT_COLOR }) });
 
 const store = createStore(reducer);
+
+store.subscribe(() => {
+  const { colors, index } = store.getState();
+  console.log(colors, index);
+})
