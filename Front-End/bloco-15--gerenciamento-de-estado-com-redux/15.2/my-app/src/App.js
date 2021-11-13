08dedd3157e4343ce6d666f774badf2da745dc10
Fernando Sena <fernandosenacruz@gmail.com>
2021-11-13 12:18:18 -0300

    ajustes

---
+++ Front-End/bloco-15--gerenciamento-de-estado-com-redux/15.2/my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FristComponent from './components/FristComponent';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <FristComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
