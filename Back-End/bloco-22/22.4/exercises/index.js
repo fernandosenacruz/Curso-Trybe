d1d30a1e822b5629e9f77f806885c7410ee42a47
Fernando Sena <fernandosenacruz@gmail.com>
2022-02-14 20:04:13 -0300

    exercicios e fixaçao

--- Back-End/bloco-22/22.4/exercises/index.js
+++ Back-End/bloco-22/22.4/exercises/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const rescue = require('express-rescue');
 
 const simpsonsUtils = require('./fs-utils');
