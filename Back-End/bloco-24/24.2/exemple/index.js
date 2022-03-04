411398f38d0d08d84a4b5e8b812fabbb6dd7b1d6
Fernando Sena <fernandosenacruz@gmail.com>
2022-03-04 19:13:18 -0300

    refact exemple, add tests

--- Back-End/bloco-24/24.2/exemple/index.js
+++ Back-End/bloco-24/24.2/exemple/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const { Address, Employee } = require('./models');
+const { Address, Employee, Book, User } = require('./models');
+
+const sequelize = new Sequelize(
+  process.env.NODE_ENV === 'test' ? config.test : config.development
+);
 
 const app = express();
 
@@ -13,6 +17,74 @@ app.get('/employees', async (_req, res) => {
   } catch (e) {
     console.log(e.message);
     res.status(500).json({ message: 'Ocorreu um erro' });
+  };
+});
+
+app.get('/employees/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const employee = await Employee.findOne({ where: { id } });
+
+    if (!employee)
+      return res.status(404).json({ message: 'Funcionário não encontrado' });
+
+    if (req.query.includeAddresses === 'true') {
+         const addresses = await Address.findAll({ where: { employeeId: id } });
+         return res.status(200).json({ employee, addresses });
+       }
+
+    return res.status(200).json(employee);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
+app.post('/employees', async (req, res) => {
+  const t = await sequelize.transaction();
+
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: t },
+    );
+
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction: t }
+    );
+
+    await t.commit();
+
+    return res.status(201).json({
+      id: employee.id, // esse dado será nossa referência para validar a transação
+      message: 'Cadastrado com sucesso'
+    });
+
+  } catch (e) {
+    await t.rollback();
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+app.get('/usersbooks/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({
+      where: { userId: id },
+      include: [{ model: Book, as: 'books', through: { attributes: [] } }],
+    });
+
+    if (!user)
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+
+    return res.status(200).json(user);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
   };
 });
 
