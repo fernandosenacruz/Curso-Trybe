50aa560e21d70a93a6132ed87997ce404d5f627b
fernandosenacruz <fernandosenacruz@gmail.com>
2022-07-12 10:39:55 -0300

    P.O.O em python

---
+++ Ciencia-da-Computacao/bl-34/34.1/exercise02.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+
+class Estatistica:
+    @classmethod
+    def media(cls, numbers):
+        return sum(numbers) / len(numbers)
+
+    @classmethod
+    def mediana(cls, numbers):
+        numbers.sort()
+        index = len(numbers) // 2
+        if len(numbers) % 2 == 0:
+            return (numbers[index - 1] + numbers[index]) / 2
+
+        return numbers[index]
+
+    @classmethod
+    def moda(cls, numbers):
+        number, _ = Counter(numbers).most_common()[0]
+        return number
